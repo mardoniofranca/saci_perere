b59e06d37083816e09fa312564ba21020841a12c
mardonio franca <mardfranca@gmail.com>
2023-10-31 10:56:29 -0300

    start

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include # new
+
+from .views import BlogListView, BlogDetailView # new
+
+urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # new 
+    path('', BlogListView.as_view(), name='home'),
+    ]
+
+
+
